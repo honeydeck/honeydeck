36e27347d8f3de557e36d42462e3b9d5cd32d259
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2026-07-01 20:23:29 +0200

    fix: welcome animation full size

--- packages/welcome-deck/layouts/WelcomeCover.tsx
+++ packages/welcome-deck/layouts/WelcomeCover.tsx
@@ -1,40 +1,55 @@
 import type { LayoutDemo, LayoutProps } from "@honeydeck/honeydeck/types";
 // @ts-expect-error no types for JSX component
 import Grainient from "../src/components/Grainient";
+import { useEffect, useState } from "react";
 
 export default function WelcomeCover({ title, children }: LayoutProps) {
-	return (
+  const [primary, setPrimary] = useState<string>('');
+  const [accent, setAccent] = useState<string>('');
+  const [surface, setSurface] = useState<string>('');
+
+  // Use theme colors for animation.
+  useEffect(() => {
+    const bodyStyles = window.getComputedStyle(document.body)
+    const primary = bodyStyles.getPropertyValue('--honeydeck-primary');
+    const accent = bodyStyles.getPropertyValue('--honeydeck-accent');
+    const surface = bodyStyles.getPropertyValue('--honeydeck-surface');
+    setPrimary(primary);
+    setAccent(accent);
+    setSurface(surface);
+  });
+
+  return (
 		<div className="grid h-full place-items-center">
-			<div className="w-full h-full" id="granientWrapper">
-				<Grainient
-					color1="#ffd93d"
-					color2="#6d4aff"
-					color3="#f3efff"
-					timeSpeed={0.1}
-					colorBalance={0}
-					warpStrength={1}
-					warpFrequency={5}
-					warpSpeed={2}
-					warpAmplitude={50}
-					blendAngle={0}
-					blendSoftness={0.05}
-					rotationAmount={500}
-					noiseScale={2}
-					grainAmount={0.06}
-					grainScale={2}
-					grainAnimated={false}
-					contrast={1.3}
-					gamma={1.5}
-					saturation={0.9}
-					centerX={0}
-					centerY={0}
-					zoom={0.9}
-				/>
-			</div>
+			<Grainient
+				color1={accent} // accent
+				color2={primary} // Primary
+				color3={surface} // surface
+				timeSpeed={0.1}
+				colorBalance={0}
+				warpStrength={1}
+				warpFrequency={5}
+				warpSpeed={2}
+				warpAmplitude={50}
+				blendAngle={0}
+				blendSoftness={0.05}
+				rotationAmount={500}
+				noiseScale={2}
+				grainAmount={0.06}
+				grainScale={2}
+				grainAnimated={false}
+				contrast={1.3}
+				gamma={1.5}
+				saturation={0.9}
+				centerX={0}
+				centerY={0}
+				zoom={0.9}
+			/>
+
 			<div className="absolute h-full w-full grid place-items-center">
-				<div className="text-center ">
+				<div className="text-center text-shadow-2xs">
 					{title && <h1>{title}</h1>}
-					<div className="text-foreground/70">{children}</div>
+					<div className="text-foreground/80 dark:text-foreground">{children}</div>
 				</div>
 			</div>
 		</div>
