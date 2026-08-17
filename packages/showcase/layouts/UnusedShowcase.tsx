bad92052189b88f6abaadc295f966b328fbbf2dc
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2026-08-17 11:10:42 +0200

    chore: format code

--- packages/showcase/layouts/UnusedShowcase.tsx
+++ packages/showcase/layouts/UnusedShowcase.tsx
@@ -4,14 +4,8 @@ export default function UnusedShowcaseLayout({ title, children }: LayoutProps) {
 	return (
 		<div className="flex h-full w-full flex-col justify-center overflow-hidden bg-surface p-[var(--honeydeck-slide-padding)] font-body text-foreground">
 			<div className="rounded-honeydeck border border-border bg-background p-16 shadow-2xl">
-				{title && (
-					<h1 className="mb-8 font-heading font-bold">
-						{title}
-					</h1>
-				)}
-				<div className="text-body text-surface-foreground">
-					{children}
-				</div>
+				{title && <h1 className="mb-8 font-heading font-bold">{title}</h1>}
+				<div className="text-body text-surface-foreground">{children}</div>
 			</div>
 		</div>
 	);
